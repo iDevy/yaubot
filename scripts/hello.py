d32556eefa05a5195feca6a19f81a822112e7b07
Jake Basile <jakebasile@me.com>
2012-11-11 10:26:10 -0600

    more greetings are now available!

--- scripts/hello.py
+++ scripts/hello.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__matcher__ = 'hello.*%NICK'
+__matcher__ = '(hello|hi|greetings).*%NICK'
 
 def respond(brain, user, message, groups):
     return 'Greetings meatbag designation %s.' % user
