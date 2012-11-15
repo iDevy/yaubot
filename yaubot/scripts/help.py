8962ac0ab79d47e74aa4da23108df841d9b6d0ea
Jake Basile <jakebasile@me.com>
2012-11-14 22:35:34 -0600

    made help a little easier to read...maybe.

--- yaubot/scripts/help.py
+++ yaubot/scripts/help.py
@@ -17,10 +17,10 @@
 __matcher__ = '%NICK.*help'
 
 def respond(brain, user, message, groups):
-    yield 'name:\t|\tactivation regex\t|\tdescription'
+    yield 'name\t| description\t| regex'
     for script in brain.bot_info['scripts']:
         name = script.__name__.split('.')[-1]
         regex = script.__matcher__.replace('%NICK', brain.bot_info['nick'])
-        doc = script.__doc__ if script.__doc__ is not None else ''
-        yield '%s:\t|\t%s\t|\t%s' % (name, regex, doc)
+        doc = script.__doc__ if script.__doc__ is not None else '\t\t'
+        yield '%s\t| %s\t| %s' % (name, doc, regex)
 
